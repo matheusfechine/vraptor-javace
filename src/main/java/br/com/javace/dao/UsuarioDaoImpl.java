5efa96b0fd8a477a76d3323e517f3cf869b70a98
Renan <renanigt@gmail.com>
2014-10-16 16:58:28 -0300

    Removendo anotação Named.

--- src/main/java/br/com/javace/dao/UsuarioDaoImpl.java
+++ src/main/java/br/com/javace/dao/UsuarioDaoImpl.java
@@ -5,12 +5,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.enterprise.context.SessionScoped;
-import javax.inject.Named;
 
 import br.com.javace.model.Usuario;
 
 @SessionScoped
-@Named("usuarioDao")
 public class UsuarioDaoImpl implements UsuarioDao, Serializable {
 	private static final long serialVersionUID = 1L;
 
